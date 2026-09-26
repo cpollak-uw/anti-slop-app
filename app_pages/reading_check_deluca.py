d60642e58ceaeaf0c6a4721904562e1aa2ac4c50
cpollak-uw <calvinpollak@gmail.com>
2026-09-26 11:52:48 -0700

    Add files via upload

---
+++ app_pages/reading_check_deluca.py
@@ -0,0 +1,4 @@
+from content.reading_check import DELUCA
+from utils.reading_check import render
+
+render(DELUCA)
---
+++ app_pages/reading_check_reinhart.py
@@ -0,0 +1,4 @@
+from content.reading_check import REINHART
+from utils.reading_check import render
+
+render(REINHART)
